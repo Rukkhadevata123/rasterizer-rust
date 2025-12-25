893d4a99caa45d543e271643a0a285455f9784f0
Rukkhadevata123 <rukkhadevata@foxmail.com>
2025-12-25 22:52:46 +0800

    prepare rewrite

--- src/io/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod config_loader;
-pub mod model_loader;
-pub mod obj_loader;
-pub mod render_settings;
-pub mod simple_cli;
--- src/ui/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod animation;
-pub mod app;
-pub mod core;
-pub mod render_ui;
-pub mod widgets;
