b0efe58e7a0832d67a188ecc1670a186c6a25156
Rukkhadevata123 <rukkhadevata@foxmail.com>
2025-12-27 17:27:55 +0800

    normal map

--- src/core/geometry.rs
+++ src/core/geometry.rs
@@ -1,24 +1,26 @@
 use nalgebra::{Point3, Vector2, Vector3};
 
 /// Represents a single vertex in 3D space.
-/// Contains position, normal vector, and texture coordinates.
 #[derive(Debug, Clone, Copy)]
 pub struct Vertex {
     /// Position in local object space.
     pub position: Point3<f32>,
     /// Normal vector for lighting calculations.
     pub normal: Vector3<f32>,
-    /// Texture coordinates (UV) for texture mapping.
+    /// Texture coordinates (UV).
     pub texcoord: Vector2<f32>,
+    /// Tangent vector (xyz) for Normal Mapping.
+    /// We use Vector3 to keep interpolation simple (ignoring handedness for now).
+    pub tangent: Vector3<f32>,
 }
 
 impl Vertex {
-    /// Creates a new vertex.
     pub fn new(position: Point3<f32>, normal: Vector3<f32>, texcoord: Vector2<f32>) -> Self {
         Self {
             position,
             normal,
             texcoord,
+            tangent: Vector3::zeros(),
         }
     }
 }
